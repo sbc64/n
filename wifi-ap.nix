a411bc8750abfab8bbfc7343a1d2f009a2d6984d
Sebastian Bolanos <sebasheston@protonmail.com>
2020-08-05 19:00:13 +0100

    Consolidated macs and ips to robert.nix

--- wifi-ap.nix
+++ wifi-ap.nix
@@ -1,41 +1,22 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs, interfaces, ... }:
 let
   secrets = import <secrets>;
-  antenna = { name = "wlan1"; mac =  "d0:37:45:6a:94:c3";};
-  embeded = { name = "wlan0"; mac =  "b8:27:eb:5e:55:31";};
   ipAddress = "10.0.0.1";
   prefixLength = 24;
   servedAddressRange = "10.0.0.2,10.0.0.50,12h";
   ssid = "kerbal_optout_nomap";
   password = secrets."uk-wifi".pw;
   repeaterBSSID = "ac:84:c6:b1:ee:d7";
-  rules = ''
-  SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="${embeded.mac}", NAME="${embeded.name}", ATTR{type}=="1"
-  SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="${antenna.mac}", NAME="${antenna.name}", ATTR{type}=="1"
-  '';
-
-  udevNetSetupLinkRules = pkgs.writeTextFile {
-    name = "80-net-setup-link.rules";
-    destination = "/etc/udev/rules.d/80-net-setup-link.rules";
-    text = ''
-      ${rules}
-      SUBSYSTEM!="net", GOTO="net_setup_link_end"
-      IMPORT{builtin}="path_id"
-      ACTION!="add", GOTO="net_setup_link_end"
-      ${rules}
-      LABEL="net_setup_link_end"
-    '';
-  };
 in {
   networking.firewall = {
-    trustedInterfaces = [ embeded.name antenna.name ];
+    trustedInterfaces = [ interfaces.embeded.name interfaces.tplink.name ];
     extraCommands = ''
-      iptables -t nat -A POSTROUTING -o ${antenna.name} -j MASQUERADE
+      iptables -t nat -A POSTROUTING -o ${interfaces.tplink.name} -j MASQUERADE
     '';
   };
 
-  networking.networkmanager.unmanaged = [ embeded.name antenna.name ];
-  networking.interfaces."${embeded.name}".ipv4.addresses = [{
+  networking.networkmanager.unmanaged = [ interfaces.embeded.name interfaces.tplink.name ];
+  networking.interfaces."${interfaces.embeded.name}".ipv4.addresses = [{
     address = ipAddress;
     prefixLength = prefixLength;
   }];
@@ -52,19 +33,19 @@ in {
 
    
   boot.kernel.sysctl = {
-    "net.ipv4.conf.${embeded.name}.forwarding" = true;
-    "net.ipv4.conf.${antenna.name}.forwarding" = true;
+    "net.ipv4.conf.${interfaces.embeded.name}.forwarding" = true;
+    "net.ipv4.conf.${interfaces.tplink.name}.forwarding" = true;
   };
 
   systemd.services.check-ap = {
-    description = "Reboots computer if the ${antenna.name} is not correct";
+    description = "Reboots computer if the ${interfaces.tplink.name} is not correct";
     wantedBy = [ "multi-user.targer" ];
     after = [ "hostapd.service" ];
     serviceConfig = {
       ExecStart = "${pkgs.bash}/bin/bash ${pkgs.writeText "check-wifi.sh" ''
-              wifi=$(${pkgs.coreutils}/bin/cat /sys/class/net/${antenna.name}/address)
-              echo "Found MAC $wifi for ${antenna.name}"
-              if [[ "$wifi" != "${antenna.mac}" ]]; then
+              wifi=$(${pkgs.coreutils}/bin/cat /sys/class/net/${interfaces.tplink.name}/address)
+              echo "Found MAC $wifi for ${interfaces.tplink.name}"
+              if [[ "$wifi" != "${interfaces.tplink.mac}" ]]; then
                  echo "rebooting..."
                  ${pkgs.systemd}/bin/systemctl reboot
               fi
@@ -79,16 +60,16 @@ in {
     path = [ pkgs.hostapd ];
     wantedBy = [ "network.target" ];
     after = [
-      "${embeded.name}-cfg.service"
+      "${interfaces.embeded.name}-cfg.service"
       "nat.service"
       "bind.service"
       "dhcpd.service"
-      "sys-subsystem-net-devices-${embeded.name}.service"
+      "sys-subsystem-net-devices-${interfaces.embeded.name}.service"
     ];
     serviceConfig = {
       ExecStart = "${pkgs.hostapd}/bin/hostapd -d ${
         pkgs.writeText "hostapd.conf" ''
-          interface=${embeded.name}
+          interface=${interfaces.embeded.name}
           driver=nl80211
           ssid=${ssid}
           hw_mode=g
@@ -111,33 +92,12 @@ in {
   services.dnsmasq = {
     enable = true;
     extraConfig = ''
-      interface=${embeded.name}
+      interface=${interfaces.embeded.name}
       listen-address=${ipAddress}
       dhcp-range=${servedAddressRange}
     '';
   };
 
-  networking.usePredictableInterfaceNames = true;
-  boot.initrd = {
-    extraUdevRulesCommands = ''
-      cp -v -f ${pkgs.systemd}/lib/udev/rules.d/75-net-description.rules $out
-      cp -v -f ${udevNetSetupLinkRules}/etc/udev/rules.d/80-net-setup-link.rules $out/
-    '';
-  };
-
-  services.udev.extraRules = rules;
-# this enabled systemd-resolved which conflicts with dnsmasq on port 53
+  # this enabled systemd-resolved which conflicts with dnsmasq on port 53
   systemd.network.enable = false; 
-  systemd.network.links = {
-    "antenna" = {
-      enable = true;
-      linkConfig = { MACAddress = antenna.mac; };
-      matchConfig = { Name = antenna.name; };
-    };
-    "embeded" = {
-      enable = true;
-      linkConfig = { MACAddress = embeded.mac; };
-      matchConfig = { Name = embeded.name; };
-    };
-  };
 }

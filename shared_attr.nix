7b515dc2d29abc6e45f6f1fe1a67ce96c7056982
Sebastian Bolanos <sebasheston@protonmail.com>
2020-08-06 10:43:41 +0100

    Add mini

--- shared_attr.nix
+++ shared_attr.nix
@@ -10,6 +10,10 @@ in
     ip = "10.100.0.3/32";
     pubkey = "zll0iq9EDn1wab+rMGoccVvV2AdHfpNJdi3RkvCcdys=";
   };
+  mini.wg = {
+    ip = "10.100.0.4/32";
+    pubkey = "HuQiO5nYdAXGG5zQX0Hqz/OsXL6tG+dk+hlMRPh9XAg=";
+  };
   bastion.wg = {
     ip = "10.100.0.1";
     publicIP = publicIP;

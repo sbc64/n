487a219312b2d0a20e44a21265f05d2bde15f1ed
Sebastian Bolanos <sebasheston@protonmail.com>
2020-11-24 06:42:46 -0600

    Update to share and wireguard

--- wireguard.nix
+++ wireguard.nix
@@ -1,17 +1,17 @@
-{ config, hostname }:
+{ config, hostname, server }:
 let
-  secrets = import <secrets>;
+  secrets = import ./secrets.nix;
   shared = import ./shared_attr.nix;
   interface = "wg0";
 in
 {
   networking.firewall.trustedInterfaces = [ interface ];
   networking.wg-quick.interfaces."${interface}" = {
-    address = [ shared."${hostname}".wg.ip ];
-    privateKey = secrets."${hostname}".wg.pk;
+    address = shared."${hostname}".wg.localIp;
+    privateKey = secrets.${hostname}.wg.pk;
     peers = [
       {
-        allowedIPs = shared.bastion.wg.allowedIPs;
+        allowedIPs = shared.${hostname}.wg.localCaptureRange;
         endpoint = shared.bastion.wg.endpoint;
         publicKey = shared.bastion.wg.pubkey;
         persistentKeepalive = 25;

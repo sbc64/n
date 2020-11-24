eaa84059d0edee15673a788e08e7eccb250d8f4e
Sebastian Bolanos <sebasheston@protonmail.com>
2020-11-24 06:54:18 -0600

    Added most complicated enabled for wireguard. But happy

--- wireguard.nix
+++ wireguard.nix
@@ -1,21 +1,23 @@
-{ config, hostname, server }:
+{ config, hostname, server, enabled ? false }:
 let
   secrets = import ./secrets.nix;
   shared = import ./shared_attr.nix;
   interface = "wg0";
-in
-{
-  networking.firewall.trustedInterfaces = [ interface ];
-  networking.wg-quick.interfaces."${interface}" = {
-    address = shared."${hostname}".wg.localIp;
-    privateKey = secrets.${hostname}.wg.pk;
-    peers = [
-      {
-        allowedIPs = shared.${hostname}.wg.localCaptureRange;
-        endpoint = shared.bastion.wg.endpoint;
-        publicKey = shared.bastion.wg.pubkey;
-        persistentKeepalive = 25;
-      }
-    ];
-  };
-}
+  result = if enabled then
+   {
+      networking.firewall.trustedInterfaces = [ interface ];
+      networking.wg-quick.interfaces."${interface}" = {
+        address = shared."${hostname}".wg.localIp;
+        privateKey = secrets.${hostname}.wg.pk;
+        peers = [
+          {
+            allowedIPs = shared.${hostname}.wg.localCaptureRange;
+            endpoint = shared.bastion.wg.endpoint;
+            publicKey = shared.bastion.wg.pubkey;
+            persistentKeepalive = 25;
+          }
+        ];
+      };
+   }
+  else {};
+in result

25e87ab1b226b4ea65448d7290e3ceb682001018
Sebastian Bolanos <sebasheston@protonmail.com>
2021-04-12 22:53:23 +0100

    Update tg image

--- tg.nix
+++ tg.nix
@@ -1,13 +1,21 @@
-{config, ... }:
+{config, pkgs ? <nixpkgs>, ... }:
 let
   network="--network=tg"; # docker network create medalla --driver bridge
   externalIp="136.244.116.42";
   rpcEth1Port="8545";
+  image="sebohe/turbo-geth";
+  tag="v2021.04.02";
+  tg = pkgs.dockerTools.pullImage {
+    imageName = image;
+    finalImageTag = tag;
+    imageDigest = "sha256:364fc9cf2ff415e3dd82a0d8d3be8d8f53808d15de1ccd06ee8c35d756f1fd9a";
+    sha256 = "000003zq2v6rrhizgb9nvhczl87lcfphq9601wcprdika2jz7qh8";
+  };
 in
 {
   docker-containers = {
     "turbo" = {
-      image = "turbo-geth:latest";
+      image = tg.imageName+":"+tag;
       extraDockerOptions = [
         ''${network}''
         ''-p=10.1.0.9:30303:30303''
@@ -26,19 +34,15 @@ in
       ];
     };
    "rpcdaemon" = {
-     image = "turbo-geth:latest";
+     image = tg.imageName+":"+tag;
      extraDockerOptions = [
        ''${network}''
      ];
-      volumes = [
-        "/mnt/tg:/blockchain"
-      ];
      cmd = [
        "rpcdaemon"
-       "--chaindata=/blockchain"
        "--http.port=${rpcEth1Port}"
        "--http.addr=0.0.0.0"
-       "--http.api=eth,net,web3"
+       "--http.api=eth,net,web3,tg"
        ''--http.corsdomain=*''
        ''--http.vhosts=*''
         "--private.api.addr=turbo:9090"
